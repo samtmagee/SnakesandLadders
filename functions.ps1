66516a26e81041cd2771b296d07d3f0d017841bb
Sam Magee <sam_t_magee@hotmail.com>
2023-11-07 01:00:51 +1300

    80's version

--- functions.ps1
+++ functions.ps1
@@ -4,6 +4,7 @@ function Invoke-Role {
     )
     Get-Random -Minimum 1 -Maximum $sides
 }
+$sleep = 500
 $board = Import-Csv C:\GITHub\SnakesandLadders\board.csv
 
 $ladderBottomLocations = $board | Where-Object { $_.'Type' -eq 'Ladder Bottom' } | Select-Object 'Location'
@@ -15,29 +16,31 @@ $snakeHeadLocations = $board | Where-Object { $_.'Type' -eq 'Snake Head' } | Sel
 
 while ($playerLocation -lt 100) {
 
-    [int]$playerRoll = Invoke-Role -sides 20
+    [int]$playerRoll = Invoke-Role -sides 6
 
     Write-Output "Rolled a $($playerRoll)"
     [int]$playerNewLocation = $playerLocation + $playerRoll
     if ($playerNewLocation -gt 100) {
         Write-Output "Roll too high ($playerNewLocation)"
-        $playerLocation = $playerNewLocation - $playerRoll
+        $playerLocation = 100 - ($playerNewLocation - 100)
+        Write-Output "Location: Bounced back to $playerLocation)"
+        Start-Sleep -Milliseconds $sleep
     }
     elseif ($ladderBottomLocations.'Location' -eq $playerNewLocation) {
         $playerNewNewLocation = $ladderTopLocations | Where-Object { $_.'Location' -eq $playerNewLocation }
         Write-Output "Location: $playerNewLocation climb the ladder to $($playerNewNewLocation.'Exit')"
         $playerLocation = $playerNewNewLocation.'Exit'
-        Start-Sleep -seconds 1
+        Start-Sleep -Milliseconds $sleep
     }
     elseif ($snakeHeadLocations.'Location' -eq $playerNewLocation) {
         $playerNewNewLocation = $snakeHeadLocations | Where-Object { $_.'Location' -eq $playerNewLocation }
         Write-Output "Location: $playerNewLocation slide down the snake to $($playerNewNewLocation.'Exit')"
         $playerLocation = $playerNewNewLocation.'Exit'
-        Start-Sleep -seconds 1
+        Start-Sleep -Milliseconds $sleep
     }
     else {
         Write-Output "Location: $playerNewLocation"
         $playerLocation = $playerNewLocation
-        Start-Sleep -seconds 1
+        Start-Sleep -Milliseconds $sleep
     }
 }
