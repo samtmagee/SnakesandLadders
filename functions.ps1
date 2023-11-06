a166aa97828bac2c210a4ee013bb8b4b60d57783
Sam Magee <sam_t_magee@hotmail.com>
2023-11-07 00:27:49 +1300

    Single player working

--- functions.ps1
+++ functions.ps1
@@ -4,8 +4,35 @@ function Invoke-Role {
     )
     Get-Random -Minimum 1 -Maximum $sides
 }
+$board = Import-Csv C:\GITHub\SnakesandLadders\board.csv
 
-for ($i = 0; $i -lt 20; $i++) {
-    Invoke-Role -sides 50
+$ladderBottomLocations = $board | Where-Object { $_.'Type' -eq 'Ladder Bottom' } | Select-Object 'Location'
+$ladderTopLocations = $board | Where-Object { $_.'Type' -eq 'Ladder Bottom' } | Select-Object 'Location','Exit'
 
+$snakeHeadLocations = $board | Where-Object { $_.'Type' -eq 'Snake Head' } | Select-Object 'Location','Exit'
+
+[int]$playerLocation = 0
+
+while ($playerLocation -lt 100) {
+
+    [int]$playerRoll = Invoke-Role
+
+    Write-Output "Rolled a $($playerRoll)"
+    [int]$playerNewLocation = $playerLocation + $playerRoll
+    if ($ladderBottomLocations.'Location' -eq $playerNewLocation) {
+        $playerNewNewLocation = $ladderTopLocations | Where-Object { $_.'Location' -eq $playerNewLocation }
+        Write-Output "Location: $playerNewLocation climb the ladder to $($playerNewNewLocation.'Exit')"
+        $playerLocation = $playerNewNewLocation.'Exit'
+        Start-Sleep 2
+    }
+    elseif ($snakeHeadLocations.'Location' -eq $playerNewLocation) {
+        $playerNewNewLocation = $snakeHeadLocations | Where-Object { $_.'Location' -eq $playerNewLocation }
+        Write-Output "Location: $playerNewLocation slide down the snake to $($playerNewNewLocation.'Exit')"
+        $playerLocation = $playerNewNewLocation.'Exit'
+        Start-Sleep 2
+    }
+    else {
+        Write-Output "Location: $playerNewLocation"
+        $playerLocation = $playerNewLocation
+    }
 }
